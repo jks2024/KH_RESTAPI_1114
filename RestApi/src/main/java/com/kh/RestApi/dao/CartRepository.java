aa0f2f7e1d68913cab6d40374b50b2184e579a6e
K.S.Jeong <jks2024@gmail.com>
2022-11-18 17:45:45 +0900

    cart add

---
+++ RestApi/src/main/java/com/kh/RestApi/dao/CartRepository.java
@@ -0,0 +1,7 @@
+package com.kh.RestApi.dao;
+
+import com.kh.RestApi.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
---
+++ RestApi/src/main/java/com/kh/RestApi/entity/Cart.java
@@ -0,0 +1,21 @@
+package com.kh.RestApi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cart")
+@Getter @Setter @ToString
+public class Cart {
+    @Id
+    @Column(name = "cart_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private MemberInfo memberInfo;
+}
