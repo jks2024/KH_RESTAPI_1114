9661d808fd3b40bb484f05c989b10b3b62ca3ab5
K.S.Jeong <jks2024@gmail.com>
2022-11-21 14:58:11 +0900

    연관관계추가

--- RestApi/src/main/java/com/kh/RestApi/entity/CartItem.java
+++ RestApi/src/main/java/com/kh/RestApi/entity/CartItem.java
@@ -14,8 +14,11 @@ public class CartItem {
     @Column(name = "cart_item_id")
     private Long id;
 
-//    @ManyToOne  // 하나의 장바구니에는 여러 개의 상품을 담을 수 있으므로 다대일 관계로 매핑
-//    @JoinColumn(name = "cart_id")
-
-
+    @ManyToOne  // 하나의 장바구니에는 여러 개의 상품을 담을 수 있으므로 다대일 관계로 매핑
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item; // 장바구니에 담을 상품의 정보를 알아야 하므로 상폼 엔티티를 매핑
+    private int count; // 같은 상품을 장바구니에 몇개 담을지 지정
 }
---
+++ RestApi/src/main/java/com/kh/RestApi/entity/OrderItem.java
@@ -0,0 +1,27 @@
+package com.kh.RestApi.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+    @ManyToOne //  한번의 주문에 여러개의 상품이 포함될 수 있으므로 다대일 매핑 관계
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne // 한번의 주문에 여러개의 상품을 주물 할 수 있으므로 다대일 매핑 관계
+    @JoinColumn(name = "order_id")
+    private Order order;
+    private int orderPrice; // 주문 가격
+    private int count; // 수량
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
